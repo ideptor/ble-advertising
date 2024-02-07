eb6919c3bd4275fae31791cac20c1fd020044237
Kim Suhyun <ideptor@gmail.com>
2024-02-08 00:37:20 +0900

    measure

--- app/src/main/java/com/example/ble_advertising/MainActivity.kt
+++ app/src/main/java/com/example/ble_advertising/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.ble_advertising
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -10,11 +11,15 @@ import com.example.ble_advertising.apmanger.DefaultFailureListener
 
 
 class MainActivity : AppCompatActivity(), APManager.OnSuccessListener {
+
+    var start = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<View>(R.id.btnTurnOn).setOnClickListener { v: View? ->
             Toast.makeText(this, "button clicked", Toast.LENGTH_LONG).show()
+            start = System.currentTimeMillis()
             val apManager: APManager? = APManager.getApManager(this)
             apManager?.turnOnHotspot(
                 this,
@@ -25,6 +30,8 @@ class MainActivity : AppCompatActivity(), APManager.OnSuccessListener {
     }
 
     override fun onSuccess(ssid: String, password: String) {
+        val durationSec = (System.currentTimeMillis()-start) / 1000.0
+        Log.e("MEASURE", "Softap duraction(sec): $durationSec")
         Toast.makeText(this, "$ssid,$password", Toast.LENGTH_LONG).show()
         startActivity(Intent(this, APDetailActivity::class.java))
     }
